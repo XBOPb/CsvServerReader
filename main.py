6f6c99fa8a0b8d22450a95d15d1b4ccbb3167f06
XBOPb <redthunder458@yandex.ru>
2023-09-16 15:35:47 +0300

    added toggle for columns

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel, QCheckBox
+from PyQt6.QtWidgets import QMainWindow, QApplication, QCheckBox, QFileDialog, QLabel, QTableWidget, QTableWidgetItem, QWidget, QVBoxLayout, QHBoxLayout
 from PyQt6.QtCore import QThread, QObject
 from PyQt6 import QtCore
 from PyQt6.QtGui import QFont
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 from interface import Ui_MainWindow
 import sqlite3
 import os
 import subprocess
 import sys
 import csv
-import pandas as pd
 
-clms = []
+
 class CSVReader(QMainWindow):
     def __init__(self):
         super(CSVReader, self).__init__()
@@ -32,9 +32,7 @@ class CSVReader(QMainWindow):
  
     def give_columns_info(self, columns):
         self.columns = columns
-        print(self.columns)
         self.ui.checkBox.setChecked(True)
-        # self.ui.data_field.setText(1234)
 
     def csv_choice_menu(self):
         file_dialog = QFileDialog()
@@ -54,10 +52,22 @@ class CSVReader(QMainWindow):
             csv_lines = list(csv.reader(file, delimiter=","))
         filename = os.path.basename(csv_path)
         temp_file = os.path.join(self.temp, filename)
+        global main_name
+        main_name = filename.split('.')[0]
         
         with open(temp_file, 'w') as file:
             file.write(str(csv_lines))
-        subprocess.run(f'curl -d @{temp_file} http://127.0.0.1:8000')
+        subprocess.check_output(f'curl -d @{temp_file} http://127.0.0.1:8000')
+        self.close()
+        self.connect_sql_qt()
+
+    def connect_sql_qt(self):
+        con = QSqlDatabase.addDatabase("QSQLITE")
+        con.setDatabaseName('db.sqlite3')
+        if not con.open():
+            print('Unable to connect')
+        win = Database(self)
+        win.show()
 
     def start_server(self):
         self.thread = QThread()
@@ -66,6 +76,52 @@ class CSVReader(QMainWindow):
         self.thread.started.connect(self.worker.start_server)
         self.thread.finished.connect(self.thread.deleteLater)
         self.thread.start()
+    
+class Database(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Table overview")
+        self.resize(450, 250)
+        self.central_widget = QWidget(self)
+        self.vertical_layout = QVBoxLayout(self.central_widget)
+        self.columns_layout = QHBoxLayout()
+        # Set up the view and load the data
+        self.view = QTableWidget()
+        self.vertical_layout.addLayout(self.columns_layout)
+        self.vertical_layout.addWidget(self.view)
+        self.view.setColumnCount(4)
+        headers = self.get_headers()
+        column_number = len(headers)
+        for header in headers:
+            self.column_box = QCheckBox(header)
+            self.column_box.setChecked(True)
+            self.column_box.stateChanged.connect(self.filter_database)
+            self.columns_layout.addWidget(self.column_box)
+
+        self.view.setHorizontalHeaderLabels(headers)
+        query = QSqlQuery(f"SELECT * FROM {main_name}")
+        while query.next():
+            rows = self.view.rowCount()
+            self.view.setRowCount(rows + 1)
+            for i in range(column_number):
+                self.view.setItem(rows, i, QTableWidgetItem(str(query.value(i))))
+        self.view.resizeColumnsToContents()
+        self.setCentralWidget(self.central_widget)
+
+    def get_headers(self):
+        connection = sqlite3.connect('db.sqlite3')
+        cursor = connection.execute(f'select * from {main_name}')
+        names = [description[0] for description in cursor.description]
+        connection.close()
+        return names
+    
+    def filter_database(self, state):
+        all_boxes = self.findChildren(QCheckBox)
+        for i, box in enumerate(all_boxes):
+            if box.checkState() == QtCore.Qt.CheckState.Unchecked:
+                self.view.setColumnHidden(i, True)
+            else:
+                self.view.setColumnHidden(i, False)
         
 
 class handler(BaseHTTPRequestHandler):
@@ -73,22 +129,21 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        message = "HTTP Server for reading csv files."
+        message = "HTTP Service for reading csv files."
         self.wfile.write(bytes(message, "utf8"))
     
     def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/csv')
-        self.end_headers()
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
         message_string = post_data.decode()
         self.message_list = eval(message_string)
         self.column_names = []
         for item in self.message_list[0]:
             self.column_names.append(item)
-        self.data = pd.DataFrame(self.message_list, columns = self.column_names)
         self.write_to_sqlite()
+        self.send_response(200)
+        self.send_header('Content-type','text/csv')
+        self.end_headers()
     
     def write_to_sqlite(self):
         connection = self.connect_to_sqlite()
@@ -99,14 +154,15 @@ class handler(BaseHTTPRequestHandler):
     
     def create_table_query(self):
         self.columns = ','.join(self.column_names)
-        create_table = f"CREATE TABLE file ({self.columns});"
+        create_table = f"CREATE TABLE IF NOT EXISTS {main_name} ({self.columns});"
+        print(create_table)
         return create_table
     
     def insert_data_query(self):
         new_list = self.message_list[1:]
-        # workaround
-        mylist = str(new_list).replace('[', '(').replace(']', ')').replace('((', '(').replace('))', ')').replace(', ()', '')
-        insert_data = f"INSERT INTO file ({self.columns}) VALUES {mylist};"
+        new_list.pop()
+        mylist = str(new_list).replace('[', '(').replace(']', ')').replace('((', '(').replace('))', ')')
+        insert_data = f"INSERT INTO {main_name} ({self.columns}) VALUES {mylist};"
         return insert_data
 
     def execute_query(self, connection, query):
@@ -124,7 +180,6 @@ class handler(BaseHTTPRequestHandler):
             db_name = 'db.sqlite3'
             current_folder = os.path.dirname(__file__)
             path = os.path.join(current_folder, db_name)
-            print(path)
             connection = sqlite3.connect(path)
             print("Connection to SQLite DB successful")
         except sqlite3.Error as e:
